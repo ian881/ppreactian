3928b436649669e2d1837491fe098ef233e521c8
ian881 <ianherrera990@gmail.com>
2023-05-22 17:31:38 -0300

    Initial commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Tienda Online</h3>
+            <div>
+            <button>motos </button>
+            <button>autos </button>
+            <button>casas </button>
+
+        </div>
+        </nav>
+    )
+}
+     
+export default NavBar
